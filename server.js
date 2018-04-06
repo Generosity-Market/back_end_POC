d60f18db668518b5208c657d693a496e77a7141d
Joseph Gordy <jgordy2424@gmail.com>
2018-04-06 17:55:06 -0400

    Added lots of boilerplate for server.js

--- server.js
+++ server.js
@@ -1,22 +1,75 @@
-const express       = require('express');
-const path          = require('path');
-const morgan        = require('morgan');
-const routes        = require('./routes/index');
-const passport      = require('passport');
-const BasicStrategy = require('passport-http').BasicStrategy;
-const bodyParser    = require('body-parser');
+const express           = require("express");
+const bodyParser        = require("body-parser");
+const path              = require("path");
+const routes            = require("./routes/index");
+const morgan            = require("morgan");
+const passport          = require('passport');
+const bcrypt            = require("bcrypt");
+const BasicStrategy     = require('passport-http').BasicStrategy;
 
 const app = express();
 
+// below line is required for production app...setting the port
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("mustache", mustacheExpress());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+app.use(passport.initialize());
+
+const authenticateUser = function(username, password, done) {
+  model.User.findOne({
+    where: {
+      'username': username.toLowerCase()
+    }
+  }).then(function (user) {
+    if (user == null) {
+      return done(null, false, { message: 'Invalid email and/or password: please try again' })
+    }
+
+    let hashedPassword = bcrypt.hashSync(password, user.salt)
+
+    if (user.password === hashedPassword) {
+      return done(null, user)
+    }
+
+    return done(null, false, { message: 'Invalid email and/or password: please try again' })
+  })
+}
+
+passport.use(new BasicStrategy(authenticateUser))
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  model.User.findOne({
+    where: {
+      'id': id
+    }
+  }).then(function (user) {
+    if (user == null) {
+      done(new Error('Wrong user id'))
+    }
+
+    done(null, user)
+  })
+})
 
 app.use('/api', routes);
 
 app.use(routes);
 
-app.listen(3000, function() {
-  console.log("App is running on localhost:3000");
-});
+// the "if" is used for testing.
+// getting the port from above..do this for production instead of localhost:3000
+if (require.main === module) {
+  app.listen(app.get('port'), function() {
+    console.log("Node app is running on port", app.get('port'));
+  });
+}
