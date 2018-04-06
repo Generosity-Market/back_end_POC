4497a78915d8844b0c44f0e28b2278ab4bbb69c3
Joseph Gordy <jgordy2424@gmail.com>
2018-04-06 18:04:11 -0400

    Added some skeleton routes to be expanded on later

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,115 @@ passport.use(new BasicStrategy(
   }
 ));
 
-router.get('/', passport.authenticate('basic', {session: false}), function(req, res) {
-  res.status(200).send('This is where I will have my API documentation');
+// Use this route for Api documentation
+router.get("/", function(req, res) {
+  console.log('<----get @ /---->');
+  console.log('req---> ', req);
+  res.status(200).send({status: "200", message: 'Everything is fine, we\'re fine', requestBody: req.body});
 });
 
+// login route
+router.post('/login', function(req, res) {
+  if ((!req.body.email) || (!req.body.password)) {
+    res.status(403).send({error: 'Fields must not be empty.'})
+  } else {
+    models.User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(function(user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+        res.status(200).send({user: user, auth_token: token});
+      } else {
+        res.status(403).send({error: "Username or password does not match."})
+      }
+    }).catch(function(err) {
+      res.status(404).send({error: err});
+    })
+  }
+});
 
+// signup route
+router.post('/signup', function(req, res) {
+  let username         = req.body.username;
+  let password         = req.body.password;
+  let email            = req.body.email;
+  let confirmPassword  = req.body.confirmPassword;
+
+  if (!username || !password) {
+    res.status(403).send({error: 'User name and password must not be blank.'})
+  }
+
+  let salt = bcrypt.genSaltSync(10)
+  let passwordHash = bcrypt.hashSync(password, salt)
+
+  let newUser = {
+    email: email,
+    username: username,
+    salt: salt,
+    password: passwordHash
+  }
+
+  if (password === confirmPassword) {
+    models.User.create(newUser)
+    .then(function(data) {
+      res.status('201').send(data);
+    })
+    .catch(function(error) {
+      res.status('400').send({error: error});
+    });
+  } else {
+    res.status('403').send({error: "Passwords do not match.", body: req.body})
+  }
+});
+
+// get all users
+router.get('/user', function(req, res) {
+  models.User.findAll({})
+  .then(function(data) {
+    res.status(200).json(data);
+  })
+  .catch(function(error) {
+    res.status(500).json(error);
+  })
+})
+
+// delete a user from the db
+//in the future we must delete associated data first
+router.delete('/user/:username', function(req, res) {
+  models.User.destroy({
+    where: {username: req.params.username}
+  })
+  .then(function(data) {
+    res.status(200).send(req.params.username + " deleted.");
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
+  });
+})
+
+// getting the entire cause list with * and *
+router.get('/causes', function(req, res) {
+  models.Cause.findAll({
+    include: [{
+       model: models.Alternate,
+       as: 'Variable',
+       include: [{
+         model: models.Like, as: "Likes"
+       }]
+     }]
+  })
+  .then(function(data) {
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).send("No causes found")
+    }
+  })
+  .catch(function(err) {
+    res.status(500).json(err);
+  })
+});
 
 module.exports = router;
