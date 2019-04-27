5efc7990cc12260d9aa2625ea34c410231ed94de
Joseph Gordy <jgordy2424@gmail.com>
2019-04-27 11:19:18 -0400

    Ignore case on incoming email addresses during login

--- services/email/templates/body.js
+++ services/email/templates/body.js
@@ -1,24 +1,28 @@
-const htmlHeader = require('./pieces/header.js');
-const htmlFooter = require('./pieces/footer.js');
+const htmlHeader = require('./pieces/header');
+const htmlFooter = require('./pieces/footer');
 const htmlStyles = require('./styles/main');
 
-const htmlBody = (contentStyles, bodyContent, mailData) => (
-    `<html>
-        <head>
-            <style type="text/css">
-                ${htmlStyles}
-                ${contentStyles}
-            </style>
-        </head>
-        <body>
-            <div class="wrapper">
-                ${htmlHeader(mailData)}
-                ${bodyContent(mailData)}
-                ${htmlFooter}
-            </div>
-        </body>
-    </html>`
-);
+const htmlBody = (
+    contentStyles,
+    bodyContent,
+    mailData,
+) => (
+        `<html>
+            <head>
+                <style type="text/css">
+                    ${htmlStyles}
+                    ${contentStyles}
+                </style>
+            </head>
+            <body>
+                <div class="wrapper">
+                    ${htmlHeader(mailData)}
+                    ${bodyContent(mailData)}
+                    ${htmlFooter}
+                </div>
+            </body>
+        </html>`
+    );
 
 
 module.exports = htmlBody;
